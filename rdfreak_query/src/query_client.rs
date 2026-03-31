7d4c31b7c90477d2d85e8d7ba17083a231ffd931
Wes Lowe <wesley.jlowe10@gmail.com>
2026-03-31 07:23:57 -0500

    added save method, added doc comments to QueryClient methods

--- rdfreak_query/src/query_client.rs
+++ rdfreak_query/src/query_client.rs
@@ -27,6 +27,7 @@ impl QueryClient {
         Self { graph_db }
     }
 
+    /// Queries the graph for a single entity of type T with the given subject.
     pub async fn query_single<T: Entity + ConstructibleEntity>(
         &self,
         entity_subject: &NamedOrBlankNode,
@@ -62,6 +63,7 @@ impl QueryClient {
         Ok(entity)
     }
 
+    /// Inserts the given entity into the graph.
     pub async fn insert<T: Entity + ConstructibleEntity>(
         &self,
         entity: &T,
@@ -79,6 +81,63 @@ impl QueryClient {
         let entity_ttl = String::from_utf8(serializer.finish().unwrap()).unwrap();
 
         let query = format!("INSERT DATA {{ {0} }}", entity_ttl);
+
+        self.graph_db
+            .update(&query)
+            .await
+            .map_err(QueryError::FailedToQueryGraph)?;
+
+        Ok(())
+    }
+
+    /// Saves the given entity to the graph.
+    ///
+    /// This is basically both an insert and a delete (upsert).
+    pub async fn save<T: Entity + ConstructibleEntity>(
+        &self,
+        entity: &T,
+    ) -> Result<(), QueryError> {
+        // this is basically both an insert and a delete.
+
+        // 1) build the query patterns
+
+        let mut construct_query_patterns = SparqlConstructQueryPatterns::new();
+        let mut variable_generator = SparqlVariableGenerator::new();
+
+        let subject_variable = variable_generator.next().unwrap();
+
+        T::build_patterns(
+            &mut construct_query_patterns,
+            &mut variable_generator,
+            &subject_variable,
+        );
+
+        // 2) serialize the entity to ttl
+
+        let mut entity_graph = Graph::new();
+
+        entity.serialize(&mut entity_graph);
+
+        let mut serializer = TurtleSerializer::new().for_writer(Vec::new());
+
+        for entity_triple in entity_graph.iter() {
+            serializer.serialize_triple(entity_triple).unwrap();
+        }
+
+        let entity_ttl = String::from_utf8(serializer.finish().unwrap()).unwrap();
+
+        // 3) build the query
+
+        let query = format!(
+            "DELETE {{ {patterns} }} INSERT {{ {entity_ttl} }} WHERE {{ VALUES {subject_var} {{ {subject_value} }} {where_patterns} }}",
+            subject_var = subject_variable,
+            subject_value = entity.get_subject(),
+            patterns = construct_query_patterns.patterns,
+            where_patterns = construct_query_patterns.where_patterns,
+            entity_ttl = entity_ttl,
+        );
+
+        // 4) execute the query
 
         self.graph_db
             .update(&query)
